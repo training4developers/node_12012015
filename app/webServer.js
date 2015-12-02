017b73245d3715fe48d49f04a19991b56608cf86
Eric W. Greene <eric@ericwgreene.com>
2015-12-01 16:25:14 -0800

    added more code demos

--- app/webServer.js
+++ app/webServer.js
@@ -6,11 +6,63 @@ module.exports = function(config) {
 		fs = require("fs"),
 		path = require("path"),
 		http = require("http"),
-		express = require("express");
+		express = require("express"),
+		bodyParser = require("body-parser");
 
 	var
 		app = express(),
-		httpServer = http.createServer(app);
+		widgetRouter = express.Router(),
+		httpServer = http.createServer(app),
+		widgets = [
+			{ id: 1, name: "Test Widget 1", color: "blue", size: "large" },
+			{ id: 2, name: "Test Widget 2", color: "red", size: "medium" },
+			{ id: 3, name: "Test Widget 3", color: "yellow", size: "small" }
+		],
+		nextWidgetId = widgets.length;
+
+	widgetRouter.route("/widgets")
+		.get(function(req, res) {
+			res.json(widgets);
+		})
+		.post(function(req, res) {
+			req.body.id = ++nextWidgetId;
+			widgets.push(req.body);
+			res.json({ newWidgetId: req.body.id });
+		});
+
+	widgetRouter.route("/widgets/:widgetId")
+		.get(function(req, res) {
+			res.json(widgets.filter(function(widget) {
+				return widget.id === parseInt(req.params.widgetId, 10);
+			})[0]);
+		})
+		.put(function(req, res) {
+
+			var widgetToUpdate = widgets.filter(function(widget) {
+				return widget.id === parseInt(req.params.widgetId, 10);
+			})[0];
+
+			widgetToUpdate.name = req.body.name;
+			widgetToUpdate.color = req.body.color;
+			widgetToUpdate.size = req.body.size;
+
+			res.status(200).end();
+
+		})
+		.delete(function(req, res) {
+
+			var widgetToDelete = widgets.filter(function(widget) {
+				return widget.id === parseInt(req.params.widgetId, 10);
+			})[0];
+
+			var indexOfWidgetToDelete = widgets.indexOf(widgetToDelete);
+
+			widgets.splice(indexOfWidgetToDelete, 1);
+			res.status(200).end();
+		});
+
+	app.use("/api", bodyParser.json());
+	app.use("/api", widgetRouter);
 
 	app.use(express.static(config.webServer.rootFolder));
 
